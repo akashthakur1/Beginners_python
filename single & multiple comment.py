7d0a4f43d5f45d896f08fd719010e1ca033397a7
akash.thakur <akash@gmail.com>
2020-07-26 23:34:28 +0530

    Single & multiple line commment

---
+++ single & multiple comment.py
@@ -0,0 +1,31 @@
+# single # use for the Single line comment
+
+
+
+""" this symbol are use for the multi line comment """
+
+print("this is akash thakur", end=' ')
+print("this is swapnil thakur")
+
+
+print("\nakash is an engineer")
+
+print("\\akash is an engineer of software industry")
+
+print("C:\"swpanil is good women in the world")
+
+
+# Commonly used Escape Sequence
+'''
+\n insert a new line in the text at that point
+\\ insert a back slash character in the text
+\" insert a double quote character in the text
+\' insert a single quote character in the text
+\t insert a tab in the text
+\b insert a backspace in the text
+\f insert a form feed in the text
+'''
+
+
+
+
