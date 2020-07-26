c9b57281e9a72125ee00a4e61fbc5b7cf334043a
akash.thakur <akash@gmail.com>
2020-07-27 00:14:05 +0530

    Datatype program with user input

---
+++ datatype program.py
@@ -0,0 +1,46 @@
+var1 = "gaon ka work"
+
+var2 = 5
+var3 = 66.5
+var4 = "automation is required "
+print(type(var1)) # print the datatype of variable
+print(type(var2))
+print(type(var3))
+
+print(var2 + var3)
+
+print((var1) + (var4))
+
+
+################################################################################
+
+#typecastig in python
+
+abc = 5
+abc2 = '45'
+abc3 = 55.65
+xyz = 65.76
+
+abc4=int(abc2)
+
+print(abc+abc4)
+print(abc+int(abc2))
+print(float(abc)+xyz)
+
+print(str(abc)+abc2)
+
+
+####################################################################################
+
+# user input
+
+print("enter the number")
+inpum = input()
+print(type(inpum))
+inpum1 = int(inpum) + 10
+print("total number of " , inpum1)
+
+
+print("total number of" , int(inpum)+ 10)
+
+
